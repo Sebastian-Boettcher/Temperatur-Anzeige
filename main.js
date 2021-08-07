19b3e884e434ff19ed7e0ce790db146f5109a495
Sebastian-Boettcher <84092041+Sebastian-Boettcher@users.noreply.github.com>
2021-08-07 18:14:51 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,42 @@
+const { response, request } = require('express');
+const express = require('express');
+const Datastore = require('nedb');
+const fetch = require('node-fetch');
+
+const app = express();
+app.listen(3000, ()=> console.log('listening on port 3000'));
+app.use(express.static('public'));
+app.use(express.json({limit: '1mb'}));
+
+
+const database = new Datastore('database.db');
+database.loadDatabase();
+
+app.get('/api', (request, response) => {
+    database.find({}, (err, data) =>{
+        if(err){
+            response.end();
+            return;
+        }
+       response.json(data); 
+    });
+});
+
+app.post('/api', (request, response) => {
+    const data = request.body;
+    const timestamp = Date.now();
+    data.timestamp = timestamp;
+    database.insert(data);
+    response.json(data);
+});
+
+app.get('/weather/:city', async (request, response) => {
+    console.log(request.params)
+    const city = request.params.city;
+   // const api_key = process.env.API_KEY;
+    const api_url = `https://api.weatherapi.com/v1/current.json?key=78f01a854bfb46c1baa190627211006&q=${city}&aqi=yes`;
+    //const api_url = `https://api.weatherapi.com/v1/current.json?key=78f01a854bfb46c1baa190627211006&q=Tholey&aqi=no`;
+    const fetch_response = await fetch(api_url);
+    const json = await fetch_response.json();
+    response.json(json);
+});
